dce2f1b1b7e0ab9450caa36f22cdfc350b62c467
Shevon Mendis <shevonmendis98@gmail.com>
2022-05-08 23:10:15 +1000

    Move vanilla react code to next app

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next';
+import { ReactElement } from 'react';
+import styled from 'styled-components';
+import { ActivateDeactivate } from '../components/ActivateDeactivate';
+import { Greeter } from '../components/Greeter';
+import { SectionDivider } from '../components/SectionDivider';
+import { SignMessage } from '../components/SignMessage';
+import { WalletStatus } from '../components/WalletStatus';
+
+const StyledAppDiv = styled.div`
+  display: grid;
+  grid-gap: 20px;
+`;
+
+const Home: NextPage = () => {
+  return (
+    <StyledAppDiv>
+      <ActivateDeactivate />
+      <SectionDivider />
+      <WalletStatus />
+      <SectionDivider />
+      <SignMessage />
+      <SectionDivider />
+      <Greeter />
+    </StyledAppDiv>
+  )
+}
+
+export default Home
