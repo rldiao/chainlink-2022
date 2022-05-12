d44b758178901ef1fd6a52fc1a695135714cc9c6
Shevon Mendis <shevonmendis98@gmail.com>
2022-05-13 01:18:54 +1000

    Create header container

---
+++ frontend/components/header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './header.module.css';
+
+export const Header = () => {
+  return (
+    <div
+        className={styles.header}
+    >
+    </div>
+  )
+};
