a29b3cbbd247b67d9f08463467d06eb5d39065ba
Shevon Mendis <shevonmendis98@gmail.com>
2022-05-25 17:08:45 +1000

    Add logic for wallet connection

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css';
+import { Web3ReactProvider } from '@web3-react/core';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import Web3 from 'web3';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Web3ReactProvider getLibrary={(provider) => new Web3(provider)}>
+      <Component {...pageProps} />;
+    </Web3ReactProvider>
+  );
 }
 
 export default MyApp;
--- frontend/pages/connect.tsx
+++ frontend/pages/connect.tsx
@@ -1,61 +1,82 @@
+import { useWeb3React } from '@web3-react/core';
 import type { NextPage } from 'next';
 import React from 'react';
 import { Button } from '../components/button/button';
 import { Header } from '../components/header/header';
-import styles from './connect.module.css';
 import { FormaticIcon } from './assets/wallet_icons/formatic';
 import { MetamaskIcon } from './assets/wallet_icons/metamask';
-import { WalletConnectIcon } from './assets/wallet_icons/wallet_conntect';
+import { WalletConnectIcon } from './assets/wallet_icons/wallet_connect';
+import styles from './connect.module.css';
+import { metamask, walletConnect, formatic } from './connectors';
 
-const Connect: NextPage = () => (
-  <>
-    <Header/>
-    <div
-        className={styles.connectDialog}
-    >
-      <h3
-          className={styles.title}
-      >
-        CONNECT WALLET
-      </h3>
-      <p
-          className={styles.instruction}
-      >
-        Select the network and wallet you want to connect
-      </p>
-      <p
-          className={styles.subtitle}
-      >
-        Wallet
-      </p>
-      <ul
-          className={styles.walletOptions}
-      >
-        <li
-            className={styles.walletOption}
-        >
-          <MetamaskIcon/> Metamask
-        </li>
-        <li
-            className={styles.walletOption}
-        >
-          <WalletConnectIcon/> Wallet Connect
-        </li>
-        <li
-            className={styles.walletOption}
-        >
-          <FormaticIcon/> Formatic
-        </li>
-      </ul>
+const Connect: NextPage = () => {
+  const { activate } = useWeb3React();
+
+  async function connect(wallet: 'metamask' | 'walletConnect' | 'formatic') {
+    const connector = wallet === 'metamask' ? metamask : (wallet === 'walletConnect' ? walletConnect : formatic);
+    try {
+      await activate(connector);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return (
+    <>
+      <Header/>
       <div
-          className={styles.buttonContainer}
+          className={styles.connectDialog}
       >
-        <Button onClick={() => {}}>
-          Proceed
-        </Button>
+        <h3
+            className={styles.title}
+        >
+          CONNECT WALLET
+        </h3>
+        <p
+            className={styles.instruction}
+        >
+          Select the network and wallet you want to connect
+        </p>
+        <p
+            className={styles.subtitle}
+        >
+          Wallet
+        </p>
+        <ul
+            className={styles.walletOptions}
+        >
+          <li
+              className={styles.walletOption}
+              role='button'
+              onClick={() => connect('metamask')}
+          >
+            <MetamaskIcon/> Metamask
+          </li>
+          <li
+              className={styles.walletOption}
+              role='button'
+              onClick={() => connect('walletConnect')}
+          >
+            <WalletConnectIcon/> Wallet Connect
+          </li>
+          <li
+              className={styles.walletOption}
+              role='button'
+              onClick={() => connect('formatic')}
+          >
+            <FormaticIcon/> Formatic
+          </li>
+        </ul>
+        <div
+            className={styles.buttonContainer}
+        >
+          <Button onClick={() => {}}>
+            Proceed
+          </Button>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Connect;
